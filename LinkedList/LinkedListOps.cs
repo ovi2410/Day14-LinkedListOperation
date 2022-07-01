2585803a86c3a69aa01c7501410fc260f512df8e
ovi2410 <guneshwarifegade@gmail.com>
2022-07-01 14:02:17 +0530

    [ovi]Added:UC4 insert item in between list

--- LinkedList/LinkedListOps.cs
+++ LinkedList/LinkedListOps.cs
@@ -9,6 +9,7 @@ namespace LinkedList
     internal class LinkedListOps
     {
         internal Node head;
+        public int count = 0;
         internal void Add(int data)
         {
             Node node = new Node(data);
@@ -29,11 +30,10 @@ namespace LinkedList
                 temp.next = node;
 
             }
-
+            count++;
             Console.WriteLine(">>> {0} is added to LinkedList...", data);
 
         }
-
         public void Append(int data)
         {
 
@@ -59,6 +59,45 @@ namespace LinkedList
             Console.WriteLine(">>> {0} is appended to LinkedList...", data);
 
 
+        }
+
+
+        internal Node InsertAtParticularPosition(int position, int data)
+        {
+            Node newNode = new Node(data);
+
+            //if (position < 1)
+            //     Console.WriteLine("Invalid position");   // this will make linked list index start from 1 explicitly
+
+            if (position == 0)
+            {
+
+                newNode.next = head;
+                head = newNode;
+            }
+            else
+            {
+                Node temp = head;
+                while (position > 1)
+                {
+                    if (position <= count)
+                    {
+                        temp = temp.next;
+                        position--;
+                    }
+                    else
+                    {
+                        Console.WriteLine("\n>> LinkedList Index out of Bound !!!\nCan not Insert {0}", data);
+                        return head;
+                    }
+                }
+                newNode.next = temp.next;
+                temp.next = newNode;
+
+            }
+
+            Console.WriteLine("\n>>> {0} is inserted at {1} position in LinkedList....\n", data, position);
+            return head;
         }
 
         internal void Display()
@@ -87,3 +126,6 @@ namespace LinkedList
 
     }
 }
+
+    
+
