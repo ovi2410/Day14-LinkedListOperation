698cbb740bb61e759c8b6fabf4195a29c168593a
ovi2410 <guneshwarifegade@gmail.com>
2022-07-01 14:36:03 +0530

    [ovi]added:uc9 delete node calculate size of linklist

--- LinkedList/Program.cs
+++ LinkedList/Program.cs
@@ -1,5 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 //Console.WriteLine("Hello, World!");
+
 using LinkedList;
 
 Console.WriteLine("----- Welcome TO LinkedList -----\n");
@@ -7,7 +8,10 @@ Console.WriteLine("----- Welcome TO LinkedList -----\n");
 LinkedListOps list = new LinkedListOps();
 list.Add(56);
 list.Add(30);
+list.Add(40);
 list.Add(70);
 list.Display();
-list.SearchandInsert(40);
+Console.WriteLine("\n>> Current Size of Linked List :- " + list.Size());
+list.DeleteElement(40);
 list.Display();
+
